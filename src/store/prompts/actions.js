94bbc207900e9f6b0c7c1acc4ce975fab02ce25a
Til-van-sluisveld <Tilvansluisveld@gmail.com>
2020-06-30 14:38:00 +0200

    Checks if data already lives in state before fetching

--- src/store/prompts/actions.js
+++ src/store/prompts/actions.js
@@ -1,5 +1,6 @@
 import Axios from "axios";
 import { apiUrl } from "../../config/constants";
+import { selectPrompts } from "./selectors";
 
 export const storePrompts = (prompts) => ({
   type: "STORE_PROMPTS",
@@ -21,10 +22,21 @@ export const getPrompts = () => async (dispatch, getState) => {
 };
 
 export const getSinglePrompt = (id) => async (dispatch, getState) => {
-  try {
-    const response = await Axios.get(`${apiUrl}/prompts/${id}`);
-    dispatch(storeSinglePrompt(response.data));
-  } catch (e) {
-    console.log(e);
+  const state = selectPrompts(getState());
+  console.log("state:", state);
+  console.log("id:", id);
+  if (state.length) {
+    //console.log("using state to find prompt");
+    const promptFound = state.find((prompt) => prompt.id === parseInt(id));
+    console.log("promptFound:", promptFound);
+    dispatch(storeSinglePrompt(promptFound));
+  } else {
+    try {
+      const response = await Axios.get(`${apiUrl}/prompts/${id}`);
+      console.log("response:", response.data);
+      dispatch(storeSinglePrompt(response.data));
+    } catch (e) {
+      console.log(e);
+    }
   }
 };

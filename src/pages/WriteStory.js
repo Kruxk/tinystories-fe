6a8108eb0f8b19e5df060e0841419d48c582ff20
Til-van-sluisveld <Tilvansluisveld@gmail.com>
2020-06-30 15:43:12 +0200

    WriteStory page has simple form with controlled inputs

--- src/pages/WriteStory.js
+++ src/pages/WriteStory.js
@@ -1,9 +1,67 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Container, Form, Button } from "react-bootstrap";
+import { useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { getSinglePrompt } from "../store/prompts/actions";
+import { selectSinglePrompt } from "../store/prompts/selectors";
 
 export default function WriteStory() {
-  return (
-    <div>
-      <h1>Writing a story</h1>
-    </div>
-  );
+  const { id } = useParams();
+  const dispatch = useDispatch();
+  const prompt = useSelector(selectSinglePrompt);
+
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+
+  function submitForm(event) {
+    event.preventDefault();
+    console.log("name:", name);
+    console.log("description:", description);
+  }
+
+  useEffect(() => {
+    dispatch(getSinglePrompt(id));
+  }, [dispatch, id]);
+
+  const render = () => {
+    return (
+      <Container>
+        <div>
+          <h1>Title: {prompt.name}</h1>
+          <p>{prompt.description}</p>
+          <Form>
+            <Form.Group controlId="formBasicName">
+              <Form.Label>Title of your Story:</Form.Label>
+              <Form.Control
+                value={name}
+                onChange={(event) => setName(event.target.value)}
+                type="text"
+                placeholder="What is your story called?"
+                required
+              />
+            </Form.Group>
+            <Form.Group controlId="formBasicTextArea">
+              <Form.Label>Write your Story:</Form.Label>
+
+              <Form.Control
+                as="textarea"
+                rows="10"
+                value={description}
+                onChange={(event) => setDescription(event.target.value)}
+                placeholder="There once was a..."
+                required
+              />
+            </Form.Group>
+            <Form.Group className="mt-5">
+              <Button variant="primary" type="submit" onClick={submitForm}>
+                Submit Story
+              </Button>
+            </Form.Group>
+          </Form>
+        </div>
+      </Container>
+    );
+  };
+
+  return <div>{Object.keys(prompt).length ? render() : <p>loading</p>}</div>;
 }

0e4e96bec9ee872aab1fb7e635d2d7c5484fb6fd
Til-van-sluisveld <Tilvansluisveld@gmail.com>
2020-06-30 11:34:41 +0200

    Promptcard now shows how many stories a prompt has

--- src/components/PromptCard/index.js
+++ src/components/PromptCard/index.js
@@ -5,6 +5,9 @@ export default function PromptCard(props) {
     <div>
       <h1>Title: {props.name}</h1>
       <p>{props.description}</p>
+      <p>
+        <i>stories:</i> {props.stories.length}
+      </p>
       <button>Read</button>
       <button>Write</button>
     </div>

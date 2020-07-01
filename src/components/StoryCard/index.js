a0fdeadbdb08ae29e4794a3162e91d8ae64ec9f9
Til-van-sluisveld <Tilvansluisveld@gmail.com>
2020-07-01 16:26:40 +0200

    Stories which belong to logged in user now have delete button

--- src/components/StoryCard/index.js
+++ src/components/StoryCard/index.js
@@ -1,10 +1,15 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { selectUser } from "../../store/user/selectors";
 
 export default function StoryCard(props) {
+  const user = useSelector(selectUser);
+
   return (
     <div>
       <h4>Title: {props.name}</h4>
       <p>{props.description}</p>
+      {props.id === user.id ? <button> Delete </button> : ""}
     </div>
   );
 }

6086195ba4d1c961ce68d26eab3042472d7cc5b0
Til-van-sluisveld <Tilvansluisveld@gmail.com>
2020-07-02 07:16:28 +0200

    if not loggedin /profile redirects to /login

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,28 +1,36 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { selectUser } from "../store/user/selectors";
+import { selectUser, selectToken } from "../store/user/selectors";
 import { Container, Col, Row } from "react-bootstrap";
 import { selectPrompts } from "../store/prompts/selectors";
 import { fetchStories, getStoriesbyUserId } from "../store/stories/actions";
 import { selectStories } from "../store/stories/selectors";
 import StoryCard from "../components/StoryCard";
 import PromptCard from "../components/PromptCard";
+import { useHistory } from "react-router-dom";
 
 export default function Profile() {
   const dispatch = useDispatch();
+  const history = useHistory();
+
   const user = useSelector(selectUser);
   const prompts = useSelector(selectPrompts).filter(
     (prompt) => prompt.userId === user.id
   );
   const stories = useSelector(selectStories);
+  const token = useSelector(selectToken);
 
   function test() {
     dispatch(getStoriesbyUserId(user.id));
   }
 
   useEffect(() => {
-    //dispatch(fetchStories());
-    dispatch(getStoriesbyUserId(user.id));
+    if (token === null) {
+      history.push("/login");
+    } else {
+      //dispatch(fetchStories());
+      dispatch(getStoriesbyUserId(user.id));
+    }
   }, [dispatch, user.id]);
 
   const oldRender = () => {
